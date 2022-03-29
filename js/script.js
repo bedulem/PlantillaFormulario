ad2fa5f9284a31447b19276d5b36df8acadd8c25
bedulem <santochibelen@gmail.com>
2022-03-28 23:12:07 -0300

    actualizando estilos

--- js/script.js
+++ js/script.js
@@ -6,5 +6,15 @@ let fecha = new Date();
 document.getElementById("nacimiento").value = fecha.toJSON().slice(0,10);
 
 
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const fechaNacimiento = document.getElementById("nacimiento");
+const telefono = document.getElementById("telefono");
+const servicio = this.document.getElementById("servicio")
+
+
+
+
 
 })
